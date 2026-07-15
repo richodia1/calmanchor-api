3a1eee0090b7e352d7af9bfcd138731297df7da5
Richard Okosodo <okosodorichard@yahoo.com>
2026-07-15 01:24:27 +0100

    initial commit

---
+++ src/main/java/com/calmanchor/calmanchor_api/CalmanchorApiApplication.java
@@ -0,0 +1,13 @@
+package com.calmanchor.calmanchor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmanchorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalmanchorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calmanchor/calmanchor_api/CalmanchorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calmanchor.calmanchor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmanchorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
